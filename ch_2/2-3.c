9adc9b92bc5133b7c1fda05611ccb93b151201ce
scott <18855480967@163.com>
2019-12-19 22:43:20 +0800

    update

---
+++ ch_2/2-3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    unsigned int i = 0;
+
+    // 当i = 0时，i--变为最大值
+    // 前面讲过 MIN_VALUE - 1 ==> MAX_VALUE
+    for (i = 9; i >= 0; i--) // i >= 0一直成立
+    {
+        printf("i = %u\n", i);
+    }
+
+    return 0;
+}
